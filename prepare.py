2be42609e7fc9455baf03673af77915aa9cc5ce2
Harshitha Rao <harshitha.ghr@gmail.com>
2021-06-21 10:25:21 +0530

    Updated prepare.py

--- prepare.py
+++ prepare.py
@@ -1,22 +1,17 @@
-# -*- coding: utf-8 -*-
-# @Last Modified by:   Harshitha Rao
-# @Last Modified time: 2020-12-05 03:27:45
-
 from sklearn.datasets import fetch_20newsgroups
 import pandas as pd
 import random
 
 def twenty_newsgroup_to_csv():
-    newsgroups_train = fetch_20newsgroups(subset='all', remove=('headers', 'footers', 'quotes'))
+    newsgroups_train = fetch_20newsgroups(subset='all', shuffle=True, random_state=42, remove=('headers', 'footers', 'quotes'))
 
     df = pd.DataFrame([newsgroups_train.data, newsgroups_train.target.tolist()]).T
     df.columns = ['text', 'target']
 
-    targets = pd.DataFrame( newsgroups_train.target_names)
+    targets = pd.DataFrame(newsgroups_train.target_names)
     targets.columns=['title']
 
     out = pd.merge(df, targets, left_on='target', right_index=True)
-    # out['date'] = pd.to_datetime('now')
 
     len_out = len(out)
     train_ix = random.sample(range(len_out), int(0.7*len_out))
@@ -27,6 +22,5 @@ def twenty_newsgroup_to_csv():
     out.iloc[train_ix].to_csv('data/training.csv', index=False, header=False)
     out.iloc[test_ix].to_csv('data/test.csv', index=False, header=False)
     out.iloc[val_ix].to_csv('data/validation.csv', index=False, header=False)
-    # out.to_csv('20_newsgroup.csv')
     
 twenty_newsgroup_to_csv()
