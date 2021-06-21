944e28224f1903c32ae771a64da623d2f4f6ba42
Harshitha Rao <harshitha.ghr@gmail.com>
2021-06-21 10:27:27 +0530

    Updated preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,17 @@
-# -*- coding: utf-8 -*-
-# @Last Modified by:   Harshitha Rao
-# @Last Modified time: 2020-12-05 03:30:45
-
 import argparse
 import os
 import csv
 import numpy as np
 import tensorflow as tf
 import nltk
+import re
+import string
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy import sparse
+from nltk.stem.wordnet import WordNetLemmatizer
 import model.data
 
 seed = 42
@@ -16,18 +20,65 @@ tf.set_random_seed(seed)
 
 csv.field_size_limit(2**28)
 
+def clean_text(sentence):
+    # remove non alphabetic sequences
+    pattern = re.compile(r'[^a-z]+')
+    sentence = sentence.lower()
+    sentence = pattern.sub(' ', sentence).strip()
+    
+    # Tokenize
+    word_list = word_tokenize(sentence)
+    
+    # stop words
+    stopwords_list = set(stopwords.words('english'))
+    # puctuation
+    punct = set(string.punctuation)
+    
+    # remove stop words
+    word_list = [word for word in word_list if word not in stopwords_list]
+    # remove very small words, length < 3
+    word_list = [word for word in word_list if len(word) > 2]
+    # remove punctuation
+    word_list = [word for word in word_list if word not in punct]
+    
+    # stemming
+    # ps  = PorterStemmer()
+    # word_list = [ps.stem(word) for word in word_list]
+    
+    # lemmatize
+    lemma = WordNetLemmatizer()
+    word_list = [lemma.lemmatize(word) for word in word_list]
+
+    # list to sentence
+    sentence = ' '.join(word_list)
+    
+    return word_list #, sentence 
 
 def tokens(text):
-    return [w.lower() for w in nltk.word_tokenize(text)]
+    return [w.lower() for w in clean_text(text)]
 
-
-def preprocess(text, vocab_to_id):
+def preprocess_tf(text, vocab_to_id):
     ids = [vocab_to_id.get(x) for x in tokens(text) if vocab_to_id.get(x)]
     if ids:
         vector = np.bincount(np.unique(ids), minlength=len(vocab_to_id))
     else:
-        vector = np.zeros(len(vocab_to_id))
-    # vector length = 2000
+        vector = np.zeros(len(vocab_to_id)) # vector length = 2000
+    return ' '.join([str(x) for x in vector])
+
+def preprocess_tfidf(text, vocab_to_id):
+    ids = [vocab_to_id.get(x) for x in tokens(text) if vocab_to_id.get(x)]
+    if ids:
+        vectorizer = TfidfVectorizer(stop_words='english', vocabulary=vocab_to_id, strip_accents='unicode')
+        response = vectorizer.fit_transform(ids)
+        n_response = response.toarray()
+        row_sum = n_response.sum(axis=1)
+        length = len(row_sum)
+        n_result = n_response/row_sum.reshape(length,1)
+        position_NaNs = np.isnan(n_result)
+        n_result[position_NaNs] = 0
+        vector = sparse.csr_matrix(n_result)
+    else:
+        vector = np.zeros(len(vocab_to_id))   
     return ' '.join([str(x) for x in vector])
 
 
@@ -48,8 +99,8 @@ def main(args):
             for y, x in data.rows(collection, num_epochs=1):
                 if y not in labels:
                     labels[y] = len(labels)
-
-                w.writerow((labels[y], preprocess(x, vocab_to_id)))
+                w.writerow((labels[y], preprocess_tf(x, vocab_to_id)))
+                # w.writerow((labels[y], preprocess_tfidf(x, vocab_to_id)))
 
     with open(os.path.join(args.output, 'labels.txt'), 'w') as f:
         f.write('\n'.join([k for k in sorted(labels, key=labels.get)]))
