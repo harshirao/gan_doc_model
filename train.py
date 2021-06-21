0617e58a88232021ce61ecb6f0c1cd096ec1920a
Harshitha Rao <harshitha.ghr@gmail.com>
2021-06-21 10:34:25 +0530

    Updated train.py

--- train.py
+++ train.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Last Modified by:   Harshitha Rao
-# @Last Modified time: 2020-12-21 08:17:42
 import os
 import argparse
 import json
@@ -30,13 +27,13 @@ def update_disc(model, x, opt, loss, params, session):
 ####### Update Generator params
 def update_gen(model, x, opt, loss, al_opt, al_loss, params, session):
     z = np.random.normal(0, 1, (params.batch_size, params.z_dim))
-    loss, _, _, _ = session.run(
+    loss, _, al_loss, _ = session.run(
                     [loss, opt, al_loss, al_opt], 
                     feed_dict={
                         model.x: x,
                         model.z: z 
                     })
-    return loss #, al_loss
+    return loss, al_loss
 
 ####### Training
 def train(model, dataset, params):
@@ -54,6 +51,8 @@ def train(model, dataset, params):
         tf.summary.scalar('g_loss', avg_g_loss)
         validation = tf.placeholder(tf.float32, [], 'validation_ph')
         tf.summary.scalar('validation', validation)
+        avg_al_loss = tf.placeholder(tf.float32, [], 'al_loss_ph')
+        tf.summary.scalar('al_loss', avg_al_loss)
 
         summary_writer = tf.summary.FileWriter(log_dir, session.graph)
         summaries = tf.summary.merge_all()
@@ -64,12 +63,13 @@ def train(model, dataset, params):
 
         d_losses = []
         g_losses = []
+        al_g_losses = []
         df_disc_losses = pd.DataFrame()
         df_gen_losses = pd.DataFrame()
         df_losses = pd.DataFrame()
         df_val_scores = pd.DataFrame()
+        df_al_losses = pd.DataFrame()
 
-        # This currently streams from disk. You set num_epochs=1 and wrap this call with something like itertools.cycle to keep this data in memory.
         training_data = dataset.batches('training', params.batch_size)
 
         best_val = 0.0
@@ -87,8 +87,8 @@ def train(model, dataset, params):
             d_loss_step = update_disc(
                             model,
                             x,
-                            model.D_solver, # model.d_opt,
-                            model.D_loss,   # model.d_loss,
+                            model.D_solver, 
+                            model.D_loss,   
                             params,
                             session
                         )
@@ -97,23 +97,28 @@ def train(model, dataset, params):
 
             ###### update generator
             g_loss_list = []
-            for i in range(params.num_gen):
-                g_loss_i = update_gen(
+            for i in range(0, params.num_gen):
+                g_loss_i, al_loss = update_gen(
                                     model,
                                     x,
-                                    model.G_solver[i],  # model.g_opt,
-                                    model.Gen_loss[i],  # model.g_loss,
+                                    model.G_solver[i],  
+                                    model.Gen_loss[i],  
                                     model.Al_solver,  
                                     model.Al_gen_loss,  
                                     params,
                                     session)
                 g_loss_list.append(g_loss_i)
+            
+            al_g_losses.append(al_loss)
+            df_al_losses = df_al_losses.append({'step': step, 'alpha_gen_loss': al_loss}, ignore_index=True)
+
             df_gen_losses = df_gen_losses.append({'step': step, 
                                     'g_0_loss': g_loss_list[0], 'g_1_loss': g_loss_list[1], 
                                     'g_2_loss': g_loss_list[2], 'g_3_loss': g_loss_list[3],
-                                    'g_4_loss': g_loss_list[4]}, ignore_index=True)
+                                    'g_4_loss': g_loss_list[4]
+                                    }, ignore_index=True)
             g_losses.append(g_loss_list)
-
+            
             ###### print discriminator and generators losses
             if step % params.log_every == 0:
                 text = '{}: {:.6f} \t'     
@@ -121,7 +126,8 @@ def train(model, dataset, params):
                 print(text.format(
                     step,
                     d_losses[-1], 
-                    ), g_losses_print[-1]   # g_losses[-1]
+                    ), g_losses_print[-1]   
+                    , al_g_losses[-1]
                 )
             
             ###### print best validation scores
@@ -166,20 +172,22 @@ def train(model, dataset, params):
                     model.z: np.random.normal(0, 1, (params.batch_size, params.z_dim)),
                     validation: val,
                     avg_d_loss: np.average(d_losses),
+                    avg_al_loss: np.average(al_g_losses),
                     avg_g_loss: np.average(g_losses)
                 })
                 summary_writer.add_summary(summary, step)
-                summary_writer.flush()
+                summary_writer.flush() 
                 d_losses = []
                 g_losses = []
 
         ###### Store and plot discriminator and generator losses
         df_losses = df_disc_losses.join(df_gen_losses.set_index('step'), on='step', how='inner')
+        df_losses = df_losses.join(df_al_losses.set_index('step'), on='step', how='inner')
         df_losses.to_csv(log_dir+'_gen_disc_losses.csv', index=False)
         df_val_scores.to_csv(log_dir+'_val_scores.csv', index=False)
 
         img_loss = df_losses.plot(   x='step'
-                                    ,y=['disc_loss', 'g_0_loss'] 
+                                    ,y=['disc_loss', 'alpha_gen_loss'] 
                                     ,kind='line'
                                     ,title='Trainings Losses: Discriminator & Generator'
                                     ,xlabel="Step"
@@ -192,7 +200,7 @@ def main(args):
         os.mkdir(args.model)
     
     if args.d_dim == 0:
-        args.d_dim = args.g_dim     # args.z_dim
+        args.d_dim = args.g_dim
 
     with open(os.path.join(args.model, args.param_file), 'w') as f:
         f.write(json.dumps(vars(args)))
@@ -200,7 +208,7 @@ def main(args):
     dataset = data.Dataset(args.dataset)
     x = tf.placeholder(tf.float32, shape=(None, args.vocab_size), name='x')
     z = tf.placeholder(tf.float32, shape=(None, args.z_dim), name='z')
-    model = m.ADM(x, z, args)
+    model = m.DocModel(x, z, args)
     train(model, dataset, args)
 
 
@@ -218,13 +226,13 @@ def parse_args():
                         help='size of generator hidden dimension')
     parser.add_argument('--g_i_dim', type=int, default=256,
                         help='size of ith generator hidden dimension')
-    parser.add_argument('--d_dim', type=int, default=0,
+    parser.add_argument('--d_dim', type=int, default=128,
                         help='size of Discriminator hidden dimension')
     parser.add_argument('--z_dim', type=int, default=100,
                         help='size of the document encoding')
     parser.add_argument('--learning_rate', type=float, default=0.0002,
                         help='initial learning rate')
-    parser.add_argument('--num_steps', type=int, default=1000,
+    parser.add_argument('--num_steps', type=int, default=10000,
                         help='the number of steps to train for')
     parser.add_argument('--batch_size', type=int, default=512,
                         help='the batch size')
